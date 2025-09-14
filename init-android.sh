432196e2bc8afd6ee3ca3ccb761334e90e153c77
Andrey Parkhomenko <andidonbas@gmail.com>
2025-09-14 19:05:51 +0300

    auto sync [14.09.2025 19:05]

---
+++ init-android.sh
@@ -0,0 +1,56 @@
+#!/bin/bash
+
+mkdir -p app/src/main/java/com/example/runcommandservice
+mkdir -p app/src/main/res/values
+mkdir -p gradle/wrapper
+
+cp AndroidManifest.xml app/src/main/
+cp MainActivity.kt app/src/main/java/com/example/runcommandservice/
+cp list app/src/main/res/values/ || cp list app/src/main/assets/
+
+curl -o gradlew https://raw.githubusercontent.com/gradle/gradle/master/gradlew
+curl -o gradlew.bat https://raw.githubusercontent.com/gradle/gradle/master/gradlew.bat
+curl -o gradle/wrapper/gradle-wrapper.jar https://raw.githubusercontent.com/gradle/gradle/master/gradle/wrapper/gradle-wrapper.jar
+curl -o gradle/wrapper/gradle-wrapper.properties https://raw.githubusercontent.com/gradle/gradle/master/gradle/wrapper/gradle-wrapper.properties
+
+chmod +x gradlew
+
+cat > app/build.gradle << 'EOF'
+plugins {
+    id 'com.android.application'
+    id 'org.jetbrains.kotlin.android'
+}
+
+android {
+    namespace 'com.example.runcommandservice'
+    compileSdk 34
+
+    defaultConfig {
+        applicationId "com.example.runcommandservice"
+        minSdk 21
+        targetSdk 34
+        versionCode 1
+        versionName "1.0"
+    }
+
+    buildTypes {
+        release {
+            minifyEnabled false
+            proguardFiles getDefaultProguardFile('proguard-android-optimize.txt'), 'proguard-rules.pro'
+        }
+    }
+    compileOptions {
+        sourceCompatibility JavaVersion.VERSION_1_8
+        targetCompatibility JavaVersion.VERSION_1_8
+    }
+    kotlinOptions {
+        jvmTarget = '1.8'
+    }
+}
+
+dependencies {
+    implementation 'androidx.core:core-ktx:1.13.1'
+    implementation 'androidx.appcompat:appcompat:1.6.1'
+    implementation 'com.google.android.material:material:1.11.0'
+}
+EOF
