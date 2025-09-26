e3bd7c34d9c32fa80aba90ed5b0ebbc58106cf7f
Andrey Parkhomenko <andidonbas@gmail.com>
2025-09-26 11:36:54 +0300

    auto sync [26.09.2025 11:36]

--- src/PermissionActivity.kt
+++ src/PermissionActivity.kt
@@ -1,5 +1,6 @@
 package com.yourcompany.yourapp5
 
+
 import android.app.Activity
 import android.content.Intent
 import android.content.pm.PackageManager
@@ -15,12 +16,7 @@ class PermissionActivity : Activity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
-        if (hasPermissions()) {
-            startMainActivity()
-        } else {
-            requestPermissions()
-        }
+        requestPermissions() // Запрашиваем разрешения при создании
     }
 
     private fun hasPermissions(): Boolean {
@@ -30,11 +26,15 @@ class PermissionActivity : Activity() {
     }
 
     private fun requestPermissions() {
-        ActivityCompat.requestPermissions(
-            this,
-            arrayOf(Manifest.permission.READ_EXTERNAL_STORAGE, Manifest.permission.WRITE_EXTERNAL_STORAGE),
-            PERMISSION_REQUEST_CODE
-        )
+        if (hasPermissions()) {
+            startMainActivity()
+        } else {
+            ActivityCompat.requestPermissions(
+                this,
+                arrayOf(Manifest.permission.READ_EXTERNAL_STORAGE, Manifest.permission.WRITE_EXTERNAL_STORAGE),
+                PERMISSION_REQUEST_CODE
+            )
+        }
     }
 
     override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
@@ -42,7 +42,7 @@ class PermissionActivity : Activity() {
         if (requestCode == PERMISSION_REQUEST_CODE && grantResults.all { it == PackageManager.PERMISSION_GRANTED }) {
             startMainActivity()
         } else {
-            Toast.makeText(this, "Разрешения требуются", Toast.LENGTH_SHORT).show()
+            Toast.makeText(this, "Требуются разрешения для работы приложения", Toast.LENGTH_LONG).show()
             finish()
         }
     }
