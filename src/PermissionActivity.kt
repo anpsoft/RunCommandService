4a68f2aa89fc41267a6dce1dc72b61a6792a58a1
Andrey Parkhomenko <andidonbas@gmail.com>
2025-09-26 11:43:48 +0300

    auto sync [26.09.2025 11:43]

--- src/PermissionActivity.kt
+++ src/PermissionActivity.kt
@@ -1,6 +1,5 @@
 package com.yourcompany.yourapp5
 
-
 import android.app.Activity
 import android.content.Intent
 import android.content.pm.PackageManager
@@ -16,7 +15,11 @@ class PermissionActivity : Activity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        requestPermissions() // Запрашиваем разрешения при создании
+        if (hasPermissions()) {
+            startMainActivity()
+        } else {
+            requestPermissions()
+        }
     }
 
     private fun hasPermissions(): Boolean {
@@ -26,15 +29,11 @@ class PermissionActivity : Activity() {
     }
 
     private fun requestPermissions() {
-        if (hasPermissions()) {
-            startMainActivity()
-        } else {
-            ActivityCompat.requestPermissions(
-                this,
-                arrayOf(Manifest.permission.READ_EXTERNAL_STORAGE, Manifest.permission.WRITE_EXTERNAL_STORAGE),
-                PERMISSION_REQUEST_CODE
-            )
-        }
+        ActivityCompat.requestPermissions(
+            this,
+            arrayOf(Manifest.permission.READ_EXTERNAL_STORAGE, Manifest.permission.WRITE_EXTERNAL_STORAGE),
+            PERMISSION_REQUEST_CODE
+        )
     }
 
     override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
