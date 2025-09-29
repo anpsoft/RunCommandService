44805471210fe2db5fe4ca9239108cedc336046f
Andrey Parkhomenko <andidonbas@gmail.com>
2025-09-29 15:42:00 +0300

    auto sync [29.09.2025 15:41]

--- src/ShortcutManager.kt
+++ src/ShortcutManager.kt
@@ -7,14 +7,12 @@ import android.content.Intent
 import android.content.pm.PackageManager
 import android.graphics.Bitmap
 import android.graphics.BitmapFactory
+import android.graphics.Canvas
+import android.graphics.Paint
 import android.util.Log
 import android.widget.Toast
 import java.io.File
 
-import android.graphics.Bitmap
-import android.graphics.BitmapFactory
-import android.graphics.Canvas
-import android.graphics.Paint
 
 
 object ShortcutManager {
